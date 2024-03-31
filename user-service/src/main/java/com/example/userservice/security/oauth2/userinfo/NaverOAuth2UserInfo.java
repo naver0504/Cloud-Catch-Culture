221037171cf96dcfbaab409d090655c49e40a66d
naver0504 <qortndwls1234@gmail.com>
2024-04-01 03:31:48 +0900

    feat: OAuth2UserInfo 관련 클래스 작성

---
+++ user-service/src/main/java/com/example/userservice/security/oauth2/userinfo/NaverOAuth2UserInfo.java
@@ -0,0 +1,40 @@
+package com.example.userservice.security.oauth2.userinfo;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class NaverOAuth2UserInfo extends OAuth2UserInfo {
+
+    public NaverOAuth2UserInfo(final Map<String, Object> attributes) {
+        super(attributes);
+        for (String s : attributes.keySet()) {
+            log.info("key: " + s + " value: " + attributes.get(s));
+        }
+    }
+
+    @Override
+    public String getId() {
+        Map<String, String> response = (Map<String, String>) attributes.get("response");
+        return (response.get("id"));
+    }
+
+    @Override
+    public String getNickname() {
+        final Map<String, String> response = (Map<String, String>) attributes.get("response");
+        return response.get("name");
+    }
+
+    @Override
+    public String getEmail() {
+        final Map<String, String> response = (Map<String, String>) attributes.get("response");
+        return response.get("email");
+    }
+
+    @Override
+    public String getProfileImageURL() {
+        final Map<String, String> response = (Map<String, String>) attributes.get("response");
+        return response.get("profile_image");
+    }
+}
