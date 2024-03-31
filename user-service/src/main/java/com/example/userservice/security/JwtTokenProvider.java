2a3032d4187c36cd59562640464276c24e8ca2ea
naver0504 <qortndwls1234@gmail.com>
2024-04-01 03:33:53 +0900

    feat: JwtTokenProvider 작성

---
+++ user-service/src/main/java/com/example/userservice/security/JwtTokenProvider.java
@@ -0,0 +1,50 @@
+package com.example.userservice.security;
+
+import com.example.userservice.entity.Role;
+import com.example.userservice.utils.CookieUtils;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+@Slf4j
+public final class JwtTokenProvider {
+
+    private static final String EMAIL = "email";
+    private static final String ROLE = "role";
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long tokenExpiration;
+
+    private SecretKey hmacShaKey;
+
+    @PostConstruct
+    protected void init() {
+        hmacShaKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(final String email, final Role role) {
+        return Jwts.builder()
+                .claim(EMAIL, email)
+                .claim(ROLE, role)
+                .signWith(hmacShaKey)
+                .expiration(Date.from(Instant.now().plusMillis(tokenExpiration)))
+                .compact();
+    }
+
+    public void sendCookie(HttpServletResponse response, String token) {
+        CookieUtils.addCookie(response, token, tokenExpiration);
+    }
+}
