e1c0d633e8efc915276a554012e524e128af60c4
naver0504 <qortndwls1234@gmail.com>
2024-04-01 03:32:50 +0900

    feat: Spring OAuth2를 이용해 소셜로그인 구현

---
+++ user-service/src/main/java/com/example/userservice/security/oauth2/handler/OAuth2LoginFailureHandler.java
@@ -0,0 +1,22 @@
+package com.example.userservice.security.oauth2.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OAuth2LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error("OAuth2 authentication failed: {}", exception.getMessage());
+        response.sendRedirect("/");
+    }
+}
