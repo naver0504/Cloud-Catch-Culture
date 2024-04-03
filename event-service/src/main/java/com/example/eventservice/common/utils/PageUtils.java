bc63611e1af54c9002e18381df3904b8f5d6f7e9
naver0504 <qortndwls1234@gmail.com>
2024-04-04 00:53:27 +0900

    feat: PageRequest static method 작성

---
+++ event-service/src/main/java/com/example/eventservice/common/utils/PageUtils.java
@@ -0,0 +1,12 @@
+package com.example.eventservice.common.utils;
+
+import org.springframework.data.domain.PageRequest;
+
+public class PageUtils {
+
+    public static final int CULTURAL_EVENT_PAGE_SIZE = 8;
+
+    public static PageRequest createEventPageRequest(int offset) {
+        return PageRequest.of(offset, CULTURAL_EVENT_PAGE_SIZE);
+    }
+}
