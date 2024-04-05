81fd7a052e2b3ad434a397b6909a1579a5b79768
naver0504 <qortndwls1234@gmail.com>
2024-04-05 17:41:51 +0900

    feat: VisitAuthRequest Entity 생성

---
+++ report-service/src/main/java/com/example/reportservice/entity/VisitAuthRequest.java
@@ -0,0 +1,42 @@
+package com.example.reportservice.entity;
+
+import com.example.reportservice.converter.StoredFileUrlConverter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class VisitAuthRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(columnDefinition = "TEXT")
+    @Convert(converter = StoredFileUrlConverter.class)
+    private List<String> storedFileUrl;
+
+    @Column(nullable = false)
+    private int userId;
+
+    @Column(nullable = false)
+    private int culturalEventId;
+
+    private LocalDateTime createdAt;
+
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+}
