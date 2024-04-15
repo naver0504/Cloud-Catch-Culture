1ad63b7957d663f4428f155f929af8647e316454
naver0504 <qortndwls1234@gmail.com>
2024-04-16 01:40:32 +0900

    feat: Kafka 설정 정보 Constant로 관리

---
+++ user-service/src/main/java/com/example/userservice/kafka/KafkaConstant.java
@@ -0,0 +1,10 @@
+package com.example.userservice.kafka;
+
+public class KafkaConstant {
+
+    public static final String BOOTSTRAP_SERVERS_CONFIG = "localhost:9092";
+    public static final String GROUP_ID = "user-service";
+    public static final String CREATE_VISIT_AUTH = "create-visit-auth";
+    public static final String ROLLBACK_VISIT_AUTH = "rollback-visit-auth";
+    public static final String AUTO_OFFSET_RESET_CONFIG = "earliest";
+}
