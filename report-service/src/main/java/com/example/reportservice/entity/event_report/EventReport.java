27fc7d595a0dbf388262b504531682482a79be34
naver0504 <qortndwls1234@gmail.com>
2024-04-29 05:00:32 +0900

    refactor: 불필요한 코드 삭제

--- report-service/src/main/java/com/example/reportservice/entity/event_report/EventReport.java
+++ report-service/src/main/java/com/example/reportservice/entity/event_report/EventReport.java
@@ -7,8 +7,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
