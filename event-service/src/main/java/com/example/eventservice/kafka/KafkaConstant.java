a9af83b816b58f0409de4400cd5d987f223f4355
naver0504 <qortndwls1234@gmail.com>
2024-05-07 01:46:18 +0900

    feat: SAGE 패턴을 위해 Topic 추가

--- event-service/src/main/java/com/example/eventservice/kafka/KafkaConstant.java
+++ event-service/src/main/java/com/example/eventservice/kafka/KafkaConstant.java
@@ -9,4 +9,8 @@ public class KafkaConstant {
     public static final String CREATE_EVENT_REPORT = "create-event-report";
     public static final String ROLLBACK_EVENT_REPORT = "rollback-event-report";
     public static final String AUTO_OFFSET_RESET_CONFIG = "earliest";
+    public static final String EVENT_REPORT_POINT = "event-report-point";
+    public static final String VISIT_AUTH_POINT = "visit-auth-point";
+    public static final String ROLLBACK_EVENT_REPORT_POINT = "rollback-event-report-point";
+    public static final String ROLLBACK_VISIT_AUTH_POINT = "rollback-visit-auth-point";
 }
