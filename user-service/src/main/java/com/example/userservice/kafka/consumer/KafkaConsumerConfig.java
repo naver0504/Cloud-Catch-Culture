dea568458a004a642e9df768e09aab78a0f7034c
naver0504 <qortndwls1234@gmail.com>
2024-05-07 01:47:41 +0900

    feat: 에러 발생 시 기본 설정으로 10번 재시도 하는 기능 수정

--- user-service/src/main/java/com/example/userservice/kafka/consumer/KafkaConsumerConfig.java
+++ user-service/src/main/java/com/example/userservice/kafka/consumer/KafkaConsumerConfig.java
@@ -1,6 +1,8 @@
 package com.example.userservice.kafka.consumer;
 
 import com.example.userservice.kafka.KafkaConstant;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -8,12 +10,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @EnableKafka
+@RequiredArgsConstructor
+@Slf4j
 public class KafkaConsumerConfig {
 
     @Bean
@@ -31,7 +39,14 @@ public class KafkaConsumerConfig {
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setCommonErrorHandler(defaultErrorHandler());
         return factory;
+    }
+
+    public DefaultErrorHandler defaultErrorHandler() {
+        return new DefaultErrorHandler((consumerRecord, exception) -> {
+            log.error("Failed to process message: {}", consumerRecord, exception);
+        }, new FixedBackOff(0L, 1L));
     }
 
 
