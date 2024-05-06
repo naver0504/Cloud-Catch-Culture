abdaf44cef594a3be36efbf9b54eb8fc79523fcc
naver0504 <qortndwls1234@gmail.com>
2024-05-07 01:54:26 +0900

    feat: findByUserIdAndCulturalEventIdAndPointChange 메소드 추가

--- user-service/src/main/java/com/example/userservice/repository/PointHistoryRepository.java
+++ user-service/src/main/java/com/example/userservice/repository/PointHistoryRepository.java
@@ -1,20 +1,16 @@
 package com.example.userservice.repository;
 
+import com.example.userservice.entity.point_history.PointChange;
 import com.example.userservice.entity.point_history.PointHistory;
-import com.example.userservice.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PointHistoryRepository extends JpaRepository<PointHistory, Integer> {
 
-    @Query("delete from PointHistory ph where ph.messageId = :messageId")
-    @Modifying
-    void deleteByMessageId(long messageId);
 
-    @Query("select ph from PointHistory ph where ph.user.id = :userId and  ph.messageId = :messageId")
-    Optional<PointHistory> findByUserIdAndPointHistoryMessageId(long userId, long messageId);
+    @Query("select ph from PointHistory ph where ph.user.id = :userId and  ph.culturalEventId = :cultureEventId and ph.pointChange = :pointChange")
+    Optional<PointHistory> findByUserIdAndCulturalEventIdAndPointChange(long userId, int cultureEventId, PointChange pointChange);
 
 }
