752d161bc0a6833c8684d597a4f5b3515ab94284
naver0504 <qortndwls1234@gmail.com>
2024-05-07 01:51:41 +0900

    feat: EventType에 토픽 필드 추가

--- report-service/src/main/java/com/example/reportservice/entity/outbox/EventType.java
+++ report-service/src/main/java/com/example/reportservice/entity/outbox/EventType.java
@@ -1,16 +1,15 @@
 package com.example.reportservice.entity.outbox;
 
 import com.example.reportservice.kafka.KafkaConstant;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum EventType {
-    VISIT_AUTH,
-    EVENT_REPORT;
+    VISIT_AUTH(KafkaConstant.CREATE_VISIT_AUTH),
+    EVENT_REPORT(KafkaConstant.CREATE_EVENT_REPORT);
 
+    private final String topic;
 
-    public String getKafkaTopic() {
-        return switch (this) {
-            case VISIT_AUTH -> KafkaConstant.CREATE_VISIT_AUTH;
-            case EVENT_REPORT -> KafkaConstant.CREATE_EVENT_REPORT;
-        };
-    }
 }
