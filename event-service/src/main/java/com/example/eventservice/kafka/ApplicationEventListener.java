a1d697668d962a23338da98fe14c1da733316500
naver0504 <qortndwls1234@gmail.com>
2024-05-07 01:42:51 +0900

    feat: SAGA 패턴 수정

--- event-service/src/main/java/com/example/eventservice/kafka/ApplicationEventListener.java
+++ event-service/src/main/java/com/example/eventservice/kafka/ApplicationEventListener.java
@@ -1,5 +1,6 @@
 package com.example.eventservice.kafka;
 
+import com.example.eventservice.kafka.message.EventReportMessage;
 import com.example.eventservice.kafka.message.VisitAuthMessage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,29 @@ public class ApplicationEventListener {
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper objectMapper;
 
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handleVisitAuthMessageBeforeCommit(final VisitAuthMessage visitAuthMessage) {
+        switch (visitAuthMessage.getTopic()) {
+            case KafkaConstant.ROLLBACK_EVENT_REPORT_POINT -> kafkaTemplate.send(KafkaConstant.ROLLBACK_VISIT_AUTH, visitAuthMessage.toString(objectMapper));
+            case KafkaConstant.CREATE_VISIT_AUTH -> kafkaTemplate.send(KafkaConstant.VISIT_AUTH_POINT, visitAuthMessage.toString(objectMapper));
+        }
+    }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
     public void handleVisitAuthMessageAfterRollback(final VisitAuthMessage visitAuthMessage) {
-        log.info("Handling VisitAuthMessage after rollback: {}", visitAuthMessage);
         kafkaTemplate.send(KafkaConstant.ROLLBACK_VISIT_AUTH, visitAuthMessage.toString(objectMapper));
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handleEventReportMessageBeforeCommit(final EventReportMessage eventReportMessage) {
+        switch (eventReportMessage.getTopic()) {
+            case KafkaConstant.ROLLBACK_EVENT_REPORT_POINT -> kafkaTemplate.send(KafkaConstant.ROLLBACK_EVENT_REPORT, eventReportMessage.toString(objectMapper));
+            case KafkaConstant.CREATE_EVENT_REPORT -> kafkaTemplate.send(KafkaConstant.EVENT_REPORT_POINT, eventReportMessage.toString(objectMapper));
+        }
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
+    public void handleEventReportMessageAfterRollback(final EventReportMessage eventReportMessage) {
+        kafkaTemplate.send(KafkaConstant.ROLLBACK_EVENT_REPORT, eventReportMessage.toString(objectMapper));
     }
 }
--- user-service/src/main/java/com/example/userservice/kafka/ApplicationEventListener.java
+++ user-service/src/main/java/com/example/userservice/kafka/ApplicationEventListener.java
@@ -2,6 +2,7 @@ package com.example.userservice.kafka;
 
 
 import com.example.userservice.entity.point_history.PointChange;
+import com.example.userservice.kafka.message.EventReportMessage;
 import com.example.userservice.kafka.message.VisitAuthMessage;
 import com.example.userservice.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,21 +27,25 @@ public class ApplicationEventListener {
     public void handleVisitAuthMessageBeforeCommit(final VisitAuthMessage visitAuthMessage) {
         log.info("Handling VisitAuthMessage before commit: {}", visitAuthMessage);
         final PointChange pointChange = PointChange.getPointChangeFromMessage(visitAuthMessage);
-        userRepository.updateUserPoint(visitAuthMessage.getUserId(), getPoint(visitAuthMessage, pointChange));
+        userRepository.updateUserPoint(visitAuthMessage.getUserId(), pointChange.getPoint());
+
     }
-
-
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
     public void handleVisitAuthMessageAfterRollback(final VisitAuthMessage visitAuthMessage) {
-        log.info("Handling VisitAuthMessage after rollback: {}", visitAuthMessage);
-        kafkaTemplate.send(KafkaConstant.ROLLBACK_VISIT_AUTH, visitAuthMessage.toString(objectMapper));
-
+        kafkaTemplate.send(KafkaConstant.ROLLBACK_VISIT_AUTH_POINT, visitAuthMessage.toString(objectMapper));
     }
 
-    private  int getPoint(final VisitAuthMessage visitAuthMessage, final PointChange pointChange) {
-        return pointChange.getPoint() * visitAuthMessage.getSign();
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handleEventReportMessageBeforeCommit(final EventReportMessage eventReportMessage) {
+        log.info("Handling EventReportMessage before commit: {}", eventReportMessage);
+        final PointChange pointChange = PointChange.getPointChangeFromMessage(eventReportMessage);
+        userRepository.updateUserPoint(eventReportMessage.getUserId(), pointChange.getPoint());
     }
 
-
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
+    public void handleEventReportMessageAfterRollback(final EventReportMessage eventReportMessage) {
+        log.info("Handling EventReportMessage after rollback: {}", eventReportMessage);
+        kafkaTemplate.send(KafkaConstant.ROLLBACK_EVENT_REPORT_POINT, eventReportMessage.toString(objectMapper));
+    }
 }
