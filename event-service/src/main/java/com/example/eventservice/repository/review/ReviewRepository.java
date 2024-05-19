9c7a834f73be3824c08a29a3fec8f25dbc9a5229
naver0504 <qortndwls1234@gmail.com>
2024-05-20 04:17:30 +0900

    feat: deleteByCulturalEventIdAndUserId 메소드 추가

--- event-service/src/main/java/com/example/eventservice/repository/review/ReviewRepository.java
+++ event-service/src/main/java/com/example/eventservice/repository/review/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.example.eventservice.repository.review;
 
 import com.example.eventservice.entity.review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -10,4 +11,8 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
 
     @Query("select r from Review r where r.culturalEvent.id = :culturalEventId and r.userId = :userId")
     Optional<Review> findByCulturalEventIdAndUserId(int culturalEventId, long userId);
+
+    @Modifying
+    @Query("delete from Review r where r.culturalEvent.id = :culturalEventId and r.userId = :userId")
+    void deleteByCulturalEventIdAndUserId(int culturalEventId, long userId);
 }
