579c6adfd3d656185a67c3b7813e27c4cf2153d7
naver0504 <qortndwls1234@gmail.com>
2024-05-20 04:13:26 +0900

    feat: default topic 일 시 return

--- event-service/src/main/java/com/example/eventservice/kafka/KafkaEventListener.java
+++ event-service/src/main/java/com/example/eventservice/kafka/KafkaEventListener.java
@@ -1,27 +1,30 @@
 package com.example.eventservice.kafka;
 
+import com.example.eventservice.common.aop.kafka.KafkaTransactional;
 import com.example.eventservice.kafka.record.KafkaResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class ApplicationEventListener {
+public class KafkaEventListener {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+
     @EventListener
     public void handleSuccessResult(final KafkaResult.SuccessResult successResult) {
         log.info("successResult: {}", successResult);
+        if(successResult.topic().equals(KafkaTransactional.DEFAULT_TOPIC)) return;
         kafkaTemplate.send(successResult.topic(), successResult.message());
     }
 
     @EventListener
     public void handleExceptionResult(final KafkaResult.ExceptionResult rollbackResult) {
         log.info("rollbackResult: {}", rollbackResult);
+        if(rollbackResult.topic().equals(KafkaTransactional.DEFAULT_TOPIC)) return;
         kafkaTemplate.send(rollbackResult.topic(), rollbackResult.message());
     }
 }
