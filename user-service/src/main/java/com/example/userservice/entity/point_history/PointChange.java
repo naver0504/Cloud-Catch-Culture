3027623ac987a0f5cec1045f3a42b8532f22b344
naver0504 <qortndwls1234@gmail.com>
2024-05-20 04:04:07 +0900

    refactor: 메소드 위치 변경

--- user-service/src/main/java/com/example/userservice/entity/point_history/PointChange.java
+++ user-service/src/main/java/com/example/userservice/entity/point_history/PointChange.java
@@ -1,8 +1,5 @@
 package com.example.userservice.entity.point_history;
 
-import com.example.userservice.kafka.message.BaseMessage;
-import com.example.userservice.kafka.message.EventReportMessage;
-import com.example.userservice.kafka.message.VisitAuthMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -17,9 +14,4 @@ public enum PointChange {
     private final String description;
     private final int point;
 
-    public static PointChange getPointChangeFromMessage(final BaseMessage message) {
-        if(message instanceof VisitAuthMessage) return VISIT_AUTH;
-        if (message instanceof EventReportMessage) return CREATE_CULTURAL_EVENT;
-        throw new IllegalArgumentException("Invalid message type");
-    }
 }
