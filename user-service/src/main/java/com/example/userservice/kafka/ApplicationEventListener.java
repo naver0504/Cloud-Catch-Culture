f8d0ee017c9746a1f174a2d01334b2b4b4aa5bf4
naver0504 <qortndwls1234@gmail.com>
2024-05-20 04:01:09 +0900

    refactor: 추상화 적용

--- user-service/src/main/java/com/example/userservice/kafka/ApplicationEventListener.java
+++ user-service/src/main/java/com/example/userservice/kafka/ApplicationEventListener.java
@@ -1,8 +1,9 @@
 package com.example.userservice.kafka;
 
 
-import com.example.userservice.entity.point_history.PointChange;
+import com.example.userservice.kafka.message.BaseMessage;
 import com.example.userservice.kafka.message.EventReportMessage;
+import com.example.userservice.kafka.message.ReviewMessage;
 import com.example.userservice.kafka.message.VisitAuthMessage;
 import com.example.userservice.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,30 +23,29 @@ public class ApplicationEventListener {
     private final ObjectMapper objectMapper;
     private final UserRepository userRepository;
 
-
     @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
-    public void handleVisitAuthMessageBeforeCommit(final VisitAuthMessage visitAuthMessage) {
-        log.info("Handling VisitAuthMessage before commit: {}", visitAuthMessage);
-        final PointChange pointChange = PointChange.getPointChangeFromMessage(visitAuthMessage);
-        userRepository.updateUserPoint(visitAuthMessage.getUserId(), pointChange.getPoint());
-
+    public void handleReviewMessageBeforeCommit(final BaseMessage message) {
+        userRepository.updateUserPoint(message.getUserId(), message.getPointChange().getPoint());
     }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
-    public void handleVisitAuthMessageAfterRollback(final VisitAuthMessage visitAuthMessage) {
-        kafkaTemplate.send(KafkaConstant.ROLLBACK_VISIT_AUTH_POINT, visitAuthMessage.toString(objectMapper));
+    public void handleReviewMessageAfterRollback(final BaseMessage message) {
+        kafkaTemplate.send(getRollbackTopic(message), message.toString(objectMapper));
     }
 
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
-    public void handleEventReportMessageBeforeCommit(final EventReportMessage eventReportMessage) {
-        log.info("Handling EventReportMessage before commit: {}", eventReportMessage);
-        final PointChange pointChange = PointChange.getPointChangeFromMessage(eventReportMessage);
-        userRepository.updateUserPoint(eventReportMessage.getUserId(), pointChange.getPoint());
+
+    public String getRollbackTopic(BaseMessage message) {
+        if(message instanceof VisitAuthMessage) {
+            return KafkaConstant.ROLLBACK_VISIT_AUTH_POINT;
+        }
+        if(message instanceof EventReportMessage) {
+            return KafkaConstant.ROLLBACK_EVENT_REPORT_POINT;
+        }
+        if(message instanceof ReviewMessage){
+            return KafkaConstant.ROLLBACK_REVIEW_POINT;
+        }
+
+        throw new IllegalArgumentException("Invalid BaseMessage Type");
     }
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_ROLLBACK)
-    public void handleEventReportMessageAfterRollback(final EventReportMessage eventReportMessage) {
-        log.info("Handling EventReportMessage after rollback: {}", eventReportMessage);
-        kafkaTemplate.send(KafkaConstant.ROLLBACK_EVENT_REPORT_POINT, eventReportMessage.toString(objectMapper));
-    }
 }
