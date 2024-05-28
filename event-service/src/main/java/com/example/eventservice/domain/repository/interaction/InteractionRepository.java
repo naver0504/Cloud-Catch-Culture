1cc21acc555b490e83ed0708a1bfb5c614d315cf
naver0504 <qortndwls1234@gmail.com>
2024-05-28 14:00:12 +0900

    feat: ID type 변경

--- event-service/src/main/java/com/example/eventservice/domain/repository/interaction/InteractionRepository.java
+++ event-service/src/main/java/com/example/eventservice/domain/repository/interaction/InteractionRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface InteractionRepository extends JpaRepository<Interaction, Long> {
+public interface InteractionRepository extends JpaRepository<Interaction, Integer> {
 
 
     @Query("delete from Interaction i where i.culturalEvent.id = :culturalEventId and i.userId = :userId and i.likeStar = :likeStar")
