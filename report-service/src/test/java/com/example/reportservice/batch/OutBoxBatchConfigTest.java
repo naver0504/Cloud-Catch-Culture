b64a164ea6a8a879f0f54d6efd9a0ca9c8e911ee
naver0504 <qortndwls1234@gmail.com>
2024-07-13 06:37:03 +0900

    feat: batch test 시 필요한 Config 설정

---
+++ report-service/src/test/java/com/example/reportservice/batch/OutBoxBatchConfigTest.java
@@ -0,0 +1,63 @@
+package com.example.reportservice.batch;
+
+import com.example.reportservice.domain.adapter.outbox.OutBoxRepository;
+import com.example.reportservice.domain.entity.outbox.EventType;
+import com.example.reportservice.domain.entity.outbox.OutBox;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.JobRepositoryTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Import(BatchTestConfiguration.class)
+@SpringBatchTest
+class OutBoxBatchConfigTest {
+
+    @Autowired
+    BeanFactory beanFactory;
+
+    @Autowired
+    JobLauncherTestUtils jobLauncherTestUtils;
+    @Autowired
+    OutBoxRepository outBoxRepository;
+    @Autowired
+    JobRepositoryTestUtils jobRepositoryTestUtils;
+
+    @AfterEach
+    void tearDown() {
+        jobRepositoryTestUtils.removeJobExecutions();
+        outBoxRepository.deleteAll();
+    }
+
+
+    @Test
+    void test() throws Exception {
+        // given
+        int MAX_SIZE = 150;
+        for (int i = 0; i < MAX_SIZE; i++) {
+            outBoxRepository.save(OutBox.builder().eventType(EventType.TEST).payload("test" + i).build());
+        }
+
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLocalDateTime("date", LocalDateTime.now())
+                .toJobParameters();
+
+        // when
+        jobLauncherTestUtils.setJob(beanFactory.getBean(OutBoxBatchConfig.BEAN_PREFIX + "_job", Job.class));
+        jobLauncherTestUtils.launchJob(jobParameters);
+        // then
+        assertEquals(0, outBoxRepository.findAll().size());
+    }
+}
